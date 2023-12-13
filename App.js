3fc74e817b116d1b77c9c25a7d3e405d7040c514
Kavya.S <115524805+Kavya-S2@users.noreply.github.com>
2023-12-13 12:13:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import DisplayMsg from './DisplayMsg';
+
+function App() {
+  return (
+    <div>
+      <header>
+      <DisplayMsg/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ DisplayMsg.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DisplayMsg(){
+return(
+    <div>
+        <h1>welcome hi</h1>
+
+    </div>
+);
+}
+
+export default DisplayMsg;
